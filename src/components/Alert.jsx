bcd557fecd149c2cf6ae578fe6a6fb26272dbd95
tanayod <tanayod741@gmail.com>
2024-06-04 16:46:05 +0700

    feat:add CSS prop

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,58 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import error from "./Icon/frown.png";
+import warning from "./Icon/alert-triangle.png";
+import info from "./Icon/alert-circle.png";
+import success from "./Icon/check-circle.png";
 
-function Alert() {}
+function Alert(props) {
+  const red = "#F9C8C8";
+  const orange = "#F9D9C8";
+  const yellow = "#F9EBC8";
+  const green = "#CEF7CD";
+  let background = "";
+  let Icon = "";
+  if (props.text === "error") {
+    background = red;
+    Icon = error;
+  } else if (props.text === "warning") {
+    background = orange;
+    Icon = warning;
+  } else if (props.text === "info") {
+    background = yellow;
+    Icon = info;
+  } else if (props.text === "success") {
+    background = green;
+    Icon = success;
+  }
+  return (
+    <div
+      css={css`
+        background-color: ${background};
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-size: 20px;
+        font-weight: 700;
+        line-height: 29.9px;
+        color: #444444;
+        border-radius: 10px;
+      `}
+    >
+      <img
+        src={Icon}
+        alt="Icon"
+        css={css`
+          margin-right: 10px;
+        `}
+      />{" "}
+      This is {props.text} alert box
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,33 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  const lightBlue = "#074ee8";
+  const DarkBlue = "#07a4e8";
+  let background = "";
+  if (props.text === "Primary") {
+    background = lightBlue;
+  } else if (props.text === "Secondary") {
+    background = DarkBlue;
+  }
+  return (
+    <button
+      css={css`
+        background-color: ${background};
+        padding: 0px 16px;
+        width: 171.19px;
+        height: 50px;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        font-weight: 400;
+        font-size: 16px;
+      `}
+    >
+      Large
+    </button>
+  );
+}
+
+export default Button;
